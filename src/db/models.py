5f486691830d897ce8cf76a67576868ef798de51
Pavel Mironov <engynear@mail.ru>
2023-05-11 18:52:04 +0300

    :card_file_box: Changed database models, add relations between resources and resourcepacks

--- src/db/models.py
+++ src/db/models.py
@@ -1,7 +1,17 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, ARRAY
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, ARRAY, Table
 from sqlalchemy.orm import relationship
 
 from db.base import Base
+
+resourcepack_models = Table('resourcepack_models', Base.metadata,
+    Column('resourcepack_id', Integer, ForeignKey('resourcepacks.id')),
+    Column('model_id', Integer, ForeignKey('models.id'))
+)
+
+resourcepack_sounds = Table('resourcepack_sounds', Base.metadata,
+    Column('resourcepack_id', Integer, ForeignKey('resourcepacks.id')),
+    Column('sound_id', Integer, ForeignKey('sounds.id'))
+)
 
 class Model(Base):
     __tablename__ = "models"
@@ -12,6 +22,11 @@ class Model(Base):
     description = Column(Text)
     tags = Column(ARRAY(String))
 
+    resourcepacks = relationship(
+        'Resourcepack',
+        secondary=resourcepack_models,
+        back_populates='models'
+    )
 
 class Sound(Base):
     __tablename__ = "sounds"
@@ -19,7 +34,13 @@ class Sound(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, index=True)
     description = Column(Text)
+    tags = Column(ARRAY(String))
 
+    resourcepacks = relationship(
+        'Resourcepack',
+        secondary=resourcepack_sounds,
+        back_populates='sounds'
+    )
 
 class Map(Base):
     __tablename__ = "maps"
@@ -36,3 +57,14 @@ class Resourcepack(Base):
     name = Column(String, index=True)
     version = Column(String)
     description = Column(Text)
+
+    models = relationship(
+        'Model',
+        secondary=resourcepack_models,
+        back_populates='resourcepacks'
+    )
+    sounds = relationship(
+        'Sound',
+        secondary=resourcepack_sounds,
+        back_populates='resourcepacks'
+    )
